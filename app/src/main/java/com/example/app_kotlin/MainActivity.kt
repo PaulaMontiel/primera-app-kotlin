1a6c54f73139eb0e4b0b437e7bdd6e0b16ea46a4
PaulaMontiel <p_dany_mr@hotmail.com>
2026-02-19 13:00:24 -0300

    Saludando a Luis

--- app/src/main/java/com/example/app_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/app_kotlin/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AppkotlinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Luis",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
